d54ebc99005e246ae7f8b3f5e07c608ce9c07597
B Pavan kumar <62787933+pavankumar468@users.noreply.github.com>
2024-03-13 15:36:59 +0530

    Add files via upload

---
+++ question-service/src/test/java/com/telsuko/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telsuko.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
